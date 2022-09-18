9cfe20fda502e164085f3adc0a1ea20fd416bb00
Guilherme Lourenço <53027213+emersontop@users.noreply.github.com>
2022-09-17 23:13:41 -0300

    atualização

---
+++ 02_JavascriptTesting/aula01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constants ={
+    error: {
+        FILE_LENGHT_ERROR_MESSAGE: "The content Lenght is invalid",
+        FILE_FIELDS_ERROR_MESSAGE: 'the provided propertis are invalid!'
+    }
+}
+
+module.exports = constants
--- 02_JavascriptTesting/aula01-mocks/src/file.js
+++ 02_JavascriptTesting/aula01-mocks/src/file.js
@@ -1,8 +1,18 @@
 const { readFile } = require('fs/promises')
 const { join } = require('path')
+const { error } = require('./constants')
+
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: ["id","name","profession","age"]
+}
+
 class File{
     static async csvToJson(filePath){
         const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
+        if(!validation.valid) throw new Error(validation.error)
+
         return content
     }
 
@@ -10,10 +20,43 @@ class File{
         const filename = join(__dirname, filePath)
         return (await readFile(filename)).toString("utf8")
     }
+    static isValid(csvString, options = DEFAULT_OPTION){
+        const [header, ...fileWithoutHeader] = csvString.split('\n') //O metódo split divide de acordo com algum parametro, ou seja aqui a quebra de linha
+        //O const [header,...fileWithoutHeader] recebe o valor e separa a primiera linha, com indice zero para a variável header e o resto será salvo em fileWhithoutHeader
+
+        // a constante isHeaderValid recebe 1 se o cabeçaljo/header for igual ao padrão passado. se for falso ele recebe 0 que chama o error da biblioteca constants
+        const isHeaderValid = header === options.fields.join(',')
+        
+        if(!isHeaderValid){
+            return{
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+                // Validando se os header estão certos! Se tiver errado aparece a mensage mde erro.
+            }
+        }
+
+        const isContentLenghtAccepted = (
+            fileWithoutHeader.length >0 && 
+            fileWithoutHeader.length <=options.maxLines
+        )
+          
+
+        if(!isContentLenghtAccepted) {
+            return{
+                error: error.FILE_LENGHT_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        return{
+            Valid: true
+        }
+    }
 }
 
 (async()=>{
-    // const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
-    const result = await File.csvToJson('./../mocks/fourItems-invalid.csv')
+    //const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
+    //const result = await File.csvToJson('./../mocks/fourItems-invalid.csv')
+    const result = await File.csvToJson('./../mocks/invalid-header.csv')
     console.log('result',result)
 })();
